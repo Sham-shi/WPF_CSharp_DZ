06fbad4d2873fbbbf9698b8abd8772a8b024fecc
ShamilShigapov <sshigapov1990@gmail.com>
2025-06-22 23:43:52 +0300

    ДЗ от 02.06.2025

---
+++ DZ_UserControl/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace DZ_UserControl;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ DZ_UserControl/UserControls/UCTest.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace DZ_UserControl.UserControls;
+
+/// <summary>
+/// Логика взаимодействия для UCTest.xaml
+/// </summary>
+public partial class UCTest : UserControl
+{
+    public required string Title { get; set; }
+    public required int MaxLength { get; set; }
+    public UCTest()
+    {
+        InitializeComponent();
+
+        DataContext = this;
+    }
+}
